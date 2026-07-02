72715d8b0d34576a970258f9f824d07684928590
Monishwaran Velmurugan <monishmurugan2224@gmail.com>
2026-07-02 16:55:42 +0530

    Added custom Open Graph social preview

--- src/components/Seo/Seo.jsx
+++ src/components/Seo/Seo.jsx
@@ -24,27 +24,38 @@ export default function Seo() {
   return (
     <Helmet>
       <html lang="en" />
+
       <title>{seo.title}</title>
+
       <meta name="description" content={seo.description} />
       <meta name="keywords" content={seo.keywords.join(', ')} />
       <meta name="author" content={siteConfig.name} />
       <meta name="robots" content="index, follow" />
+
       <link rel="canonical" href={canonicalUrl} />
 
+      {/* Open Graph */}
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="en_IN" />
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:url" content={canonicalUrl} />
       <meta property="og:image" content={seo.ogImage} />
-      <meta property="og:site_name" content={siteConfig.name} />
+      <meta property="og:image:secure_url" content={seo.ogImage} />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:site_name" content="Monishwaran Velmurugan Portfolio" />
 
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.ogImage} />
 
-      <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
+      <script type="application/ld+json">
+        {JSON.stringify(jsonLd)}
+      </script>
     </Helmet>
   )
 }
